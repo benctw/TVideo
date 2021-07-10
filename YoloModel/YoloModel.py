b3f524625772e7c5d65466fdb64bd757d8c45690
toy00415 <toy890415@gmail.com>
2021-07-10 21:16:56 +0800

    Update YoloModel.py

--- YoloModel/YoloModel.py
+++ YoloModel/YoloModel.py
@@ -104,7 +104,46 @@ class YoloModel(CVModel):
 	# 矯正
 	@staticmethod
 	def correct():
-		pass
+		img = cv2.imread('PL.jpg')
+		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		blurred = cv2.GaussianBlur(gray, (11, 11), 0)
+		edged = cv2.Canny(blurred, 20, 160)          # 边缘检测
+
+		cnts,_ = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # 轮廓检测
+		#cnts = cnts[0] if imutils.is_cv2() else cnts[1]  # 判断是opencv2还是opencv3
+		docCnt = None
+
+		if len(cnts) > 0:
+			cnts = sorted(cnts, key=cv2.contourArea, reverse=True) # 根据轮廓面积从大到小排序
+			for c in cnts:
+				peri = cv2.arcLength(c, True)                         # 计算轮廓周长
+				approx = cv2.approxPolyDP(c, 0.02*peri, True)         # 轮廓多边形拟合
+				# 轮廓为4个点表示找到纸张
+				if len(approx) == 4:
+					docCnt = approx
+					break
+		for peak in docCnt:
+			peak = peak[0]
+			cv2.circle(img, tuple(peak), 10, (255, 0, 0))
+			
+		H, W = img.shape[:2]
+
+		point_set_0 = np.float32([docCnt[1,0],docCnt[2,0],docCnt[3,0],docCnt[0,0]])
+		point_set_1 = np.float32([[0, 0],[0, 140],[440, 140],[440, 0]])
+
+		# 变换矩阵
+		mat = cv2.getPerspectiveTransform(point_set_0, point_set_1)
+		# 投影变换
+		lic = cv2.warpPerspective(img, mat, (440, 140))
+
+		# 显示校正后的车牌
+		cv2.namedWindow("img")
+		cv2.imshow("img", img)
+		cv2.namedWindow("Lic")
+		cv2.imshow("Lic", lic)
+		# 暂停、关闭窗口
+		cv2.waitKey(0)
+		cv2.destroyAllWindows()
 
 	# 獲得車牌號碼
 	@staticmethod
